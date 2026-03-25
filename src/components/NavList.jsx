69f2f53e0c87ebb396d110abf7bf36b4165928b8
lnmrn <lnmrn.dev@gmail.com>
2026-03-25 15:05:27 +0100

    small refactor

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -1,33 +1,12 @@
+import NavLink from "./NavLink";
+
 function NavList() {
   return (
     <ul className="nav-links" id="nav-links">
-      <li>
-        <a href="#home" className="nav-link">
-          {" "}
-          home{" "}
-        </a>
-      </li>
-
-      <li>
-        <a href="#about" className="nav-link">
-          {" "}
-          about{" "}
-        </a>
-      </li>
-
-      <li>
-        <a href="#services" className="nav-link">
-          {" "}
-          services{" "}
-        </a>
-      </li>
-
-      <li>
-        <a href="#tours" className="nav-link">
-          {" "}
-          tours
-        </a>
-      </li>
+      <NavLink linkRef="#home">home</NavLink>
+      <NavLink linkRef="#about">about</NavLink>
+      <NavLink linkRef="#services">services</NavLink>
+      <NavLink linkRef="#tours">tours</NavLink>
     </ul>
   );
 }
