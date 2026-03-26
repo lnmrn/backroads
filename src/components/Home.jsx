b9bbd3b38b50df58d883a894149af75f16e27d1c
lnmrn <lnmrn.dev@gmail.com>
2026-03-26 14:57:36 +0100

    fixed image path in css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <section className="hero" id="home">
       <div className="hero-banner">
-        <h1>continue exploring</h1>
+        <h1>Your Backroads</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
           explicabo debitis est autem dicta.
