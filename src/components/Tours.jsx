4f29f4106b8c1cd1eb75733fd7d3afcbb1da6109
lnmrn <lnmrn.dev@gmail.com>
2026-04-17 16:36:12 +0200

    Removed unnecessary comments I forgot to remove like 5 commits already

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -33,54 +33,6 @@ function Tours() {
             shortInfo={tour.info.slice(0, 100)}
           />
         ))}
-        {/* <Tour
-          image={tour1}
-          date="august 26th, 2020"
-          title="Tibet Adventure"
-          location="china"
-          duration={6}
-          price={2100}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae
-          tempore voluptatum maxime reprehenderit eum quod exercitationem fugit,
-          qui corporis.
-        </Tour>
-        <Tour
-          image={tour2}
-          date="october 1th, 2020"
-          title="best of java"
-          location="indonesia"
-          duration={11}
-          price={1400}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae
-          tempore voluptatum maxime reprehenderit eum quod exercitationem fugit,
-          qui corporis.
-        </Tour>
-        <Tour
-          image={tour3}
-          date="september 15th, 2020"
-          title="explore hong kong"
-          location="hong kong"
-          duration={8}
-          price={5000}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae
-          tempore voluptatum maxime reprehenderit eum quod exercitationem fugit,
-          qui corporis.
-        </Tour>
-        <Tour
-          image={tour4}
-          date="december 5th, 2019"
-          title="kenya highlights"
-          location="kenya"
-          duration={20}
-          price={3300}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae
-          tempore voluptatum maxime reprehenderit eum quod exercitationem fugit,
-          qui corporis.
-        </Tour> */}
       </div>
     </section>
   );
