e7f724991f0d80488f398820eba1e0547f12fc44
lnmrn <lnmrn.dev@gmail.com>
2026-04-17 16:38:20 +0200

    Remove old imports and static assets

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,9 +1,5 @@
 import Heading from "./Heading";
 import Tour from "./Tour";
-import tour1 from "../assets/tour-1.jpeg";
-import tour2 from "../assets/tour-2.jpeg";
-import tour3 from "../assets/tour-3.jpeg";
-import tour4 from "../assets/tour-4.jpeg";
 import { randomFutureDate } from "../helper";
 import { useFetchTours } from "../hooks/useFetchTours";
 import { useState } from "react";
