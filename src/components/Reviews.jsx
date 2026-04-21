c8fcf727c92e8a67b05f0c5d0577f8440223536d
lnmrn <lnmrn.dev@gmail.com>
2026-04-21 14:06:17 +0200

    Add reviews feature

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { reviews } from "../data";
+import Heading from "./Heading";
+
+function Reviews() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const { name, tour, image, text } = reviews[currentIndex];
+
+  function handleNext() {
+    setCurrentIndex((index) => (index >= reviews.length - 1 ? 0 : index + 1));
+  }
+
+  function handlePrevious() {
+    setCurrentIndex((index) => (index <= 0 ? reviews.length - 1 : index - 1));
+  }
+
+  return (
+    <section className="section services" id="reviews">
+      <Heading title="Our" highlight="reviews" />
+      <div className="review">
+        <div className="review-card">
+          <div className="img-container">
+            <img src={image} className="person-img" />
+          </div>
+
+          <h3>{name}</h3>
+          <h4 className="review-tour">{tour}</h4>
+          <p>{text}</p>
+          <div className="btn-container">
+            <button className="prev-btn" onClick={handlePrevious}>
+              <i className="fas fa-less-than"></i>
+            </button>
+
+            <button onClick={handleNext} className="next-btn">
+              <i className="fas fa-greater-than"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Reviews;
