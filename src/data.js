2561b2c0acb533aaee2e7993d086f45b09bd590c
lnmrn <lnmrn.dev@gmail.com>
2026-04-21 14:05:03 +0200

    Add reviews feature

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ const navLinks = [
   { id: 2, href: "#about", content: "about" },
   { id: 3, href: "#services", content: "services" },
   { id: 4, href: "#tours", content: "tours" },
+  { id: 5, href: "#reviews", content: "reviews" },
 ];
 
 const externalLinks = [
@@ -11,4 +12,35 @@ const externalLinks = [
   { id: 3, href: "https://www.squarespace.com", icon: "fab fa-squarespace" },
 ];
 
-export { navLinks, externalLinks };
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    tour: "Best of Paris",
+    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    tour: "Best of Ireland",
+    image: "https://www.course-api.com/images/people/person-2.jpeg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    tour: "Best of Rome",
+    image: "https://www.course-api.com/images/people/person-4.jpeg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    tour: "Best of Poland",
+    image: "https://www.course-api.com/images/people/person-3.jpeg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+export { navLinks, externalLinks, reviews };
