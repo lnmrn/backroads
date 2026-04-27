7df4bba33545c145fc42f444450568f1cc190a67
lnmrn <lnmrn.dev@gmail.com>
2026-04-27 13:57:57 +0200

    Add a feature to only keep one element open at the time

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
 
-function Faq({ faq }) {
-  const [isShowing, setIsShowing] = useState(false);
+function Faq({ faq, currId, handleToggle }) {
   const { id, question, answer } = faq;
+
+  const isCurrent = id === currId;
 
   return (
     <li>
-      <h4 className={isShowing ? "show" : ""}>
+      <h4 className={isCurrent ? "show" : ""}>
         {question}{" "}
-        <button
-          className="faq-btn"
-          onClick={() => {
-            setIsShowing((s) => !s);
-          }}
-        >
-          <i className="fas fa-plus-circle pulse"></i>
+        <button className="faq-btn" onClick={() => handleToggle(id)}>
+          {!isCurrent ? (
+            <i className="fas fa-plus-circle pulse"></i>
+          ) : (
+            <i className="fas fa-minus-circle"></i>
+          )}
         </button>
       </h4>
-      <p className={isShowing ? "open" : ""}>{isShowing && answer}</p>
+      <p className={isCurrent ? "open" : ""}>{isCurrent && answer}</p>
     </li>
   );
 }
--- src/components/FaqList.jsx
+++ src/components/FaqList.jsx
@@ -4,13 +4,25 @@ import Faq from "./Faq";
 import Heading from "./Heading";
 
 function FaqList() {
+  const [currId, setCurrId] = useState(null);
+
+  function handleToggle(id) {
+    const activeId = id === currId ? null : id;
+    setCurrId(activeId);
+  }
+
   return (
     <section className="section" id="faq">
       <Heading title="More" highlight="info" />
       <div className="faq-container">
         <ul className="faq">
           {faqData.map((f) => (
-            <Faq key={f.id} faq={f} />
+            <Faq
+              key={f.id}
+              faq={f}
+              currId={currId}
+              handleToggle={handleToggle}
+            />
           ))}
         </ul>
       </div>
